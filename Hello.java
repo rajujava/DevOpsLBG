cbdb0399660080c69863e3542d805d0f73918efe
Raju Kavikondala <itskraju@gmail.com>
2020-01-08 19:05:08 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
  {
     public static void main(String args[])
      {
-      System.out.println("Welcome to LBG");
+      System.out.println("Welcome to LBG DevOps session");
       }
  }     
